eef6b57f65782058ef7d3b27d10fc10e71d22811
xinxin <1105145869@qq.com>
2021-08-06 17:27:50 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -11,21 +11,29 @@
   <div @click="add">
     ++{{ count }}
   </div>
+  <div>{{ name }}</div>
+  <div>{{ age }}</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, reactive, toRefs } from 'vue'
 import { useRouter } from 'vue-router'
 import demo from '@/hooks/dome'
 export default defineComponent({
   setup () {
+    const initData = {
+      name: 'lin',
+      age: 18
+    }
     const router = useRouter()
     const title = ref('home')
     const { count, add } = demo()
+    const data = reactive({ ...initData })
     const handleClick = () => {
       router.push('/member')
     }
     return {
+      ...toRefs(data),
       title,
       handleClick,
       count,
